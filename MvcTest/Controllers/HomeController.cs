b13bc9905b63071c32a5f3055d1004ab3b18deff
Steve Townend <steve@townendmail.co.uk>
2017-03-12 09:07:01 +0000

    Should be good now

--- MvcTest/Controllers/HomeController.cs
+++ MvcTest/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace MvcTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.h";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.h";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
