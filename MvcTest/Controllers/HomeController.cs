055dc1bf55bd374835bc729c83e07694d6bac599
Steve Townend <steve@townendmail.co.uk>
2017-03-24 08:19:27 +0000

    Fix

--- MvcTest/Controllers/HomeController.cs
+++ MvcTest/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace MvcTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.HH";
+            ViewBag.Message = "Your application description page.";
             
 
             return View();
