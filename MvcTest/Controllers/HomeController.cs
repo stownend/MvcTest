d1286d3d1046c132152aec40363b4c0ebf3641c9
Steve Townend <steve@townendmail.co.uk>
2017-03-24 06:47:45 +0000

    Breaking test

--- MvcTest/Controllers/HomeController.cs
+++ MvcTest/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace MvcTest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page.hh";
             
 
             return View();
